53f9b80e2168cbe224986940f2ae41f732402e1e
Charles Huang <hzzgood@gmail.com>
2022-06-28 22:14:25 +1000

    remove some unused code

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -111,39 +111,8 @@ export class CdkStack extends cdk.Stack {
       scaleOutCooldown: cdk.Duration.seconds(60)
     });
 
-    // *** PIPELINE CONSTRUCTS ***
-    // const deployAction = new codepipeline_actions.EcsDeployAction({
-    //   actionName: 'DeployAction',
-    //   service: fargateService.service,
-    //   // imageFile: new codepipeline.ArtifactPath(buildOutput, `imagedefinitions.json`)
-    // });
-
-    // *** PIPELINE STAGES ***
-    // new codepipeline.Pipeline(this, 'MyECSPipeline', {
-    //   stages: [
-    //     // {
-    //     //   stageName: 'Source',
-    //     //   actions: [sourceAction],
-    //     // },
-    //     // {
-    //     //   stageName: 'Build',
-    //     //   actions: [buildAction],
-    //     // },
-    //     // {
-    //     //   stageName: 'Approve',
-    //     //   actions: [manualApprovalAction],
-    //     // },
-    //     {
-    //       stageName: 'Deploy-to-ECS',
-    //       actions: [deployAction],
-    //     }
-    //   ]
-    // });
-
     //OUTPUT
     new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
 
-    // @ts-ignore
-    // return fargateService.service;
   }
 }
